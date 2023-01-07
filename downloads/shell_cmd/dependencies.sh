0f7239928dc109e2735ba70183bb43aeffcea55e
meong92 <38969204+meong92@users.noreply.github.com>
2023-01-07 21:05:42 +0700

    up

--- downloads/shell_cmd/dependencies.sh
+++ downloads/shell_cmd/dependencies.sh
@@ -1,7 +1,12 @@
-sudo apt install python3
+sudo apt install python3.10
 pip install pyinstaller
 
 pip install escpos
 pip install configparser
 
 pip install PyQt5
+
+pip install opencv-python
+
+
+pip install postgres
